a84771eaa6224ec4b76aff56bd88764a4cc8ef6e
Kev <cloneofghosts@users.noreply.github.com>
2024-10-14 17:06:55 -0400

    Change long description to be markdown

--- setup.py
+++ setup.py
@@ -29,5 +29,6 @@ setup(
     packages=["pirateweather"],
     package_data={"pirateweather": ["LICENSE.txt", "README.md"]},
     long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     install_requires=["requests==2.32.3"],
 )
